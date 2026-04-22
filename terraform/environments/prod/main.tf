b26f2068d1095207e80f77bdc9a29199319c10bd
B Uma Maheswar Reddy <Umareddyb1994@gmail.com>
2026-04-22 11:23:59 +0530

    Create main.tf

---
+++ terraform/environments/prod/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source   = "../../modules/vpc"
+  vpc_cidr = var.vpc_cidr
+}
+
+module "eks" {
+  source       = "../../modules/eks"
+  cluster_name = var.cluster_name
+  subnet_ids   = module.vpc.private_subnets
+}
+
+module "ecr" {
+  source    = "../../modules/ecr"
+  repo_name = var.repo_name
+}
